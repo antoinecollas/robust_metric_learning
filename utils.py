6245264811e17c2d9a98b92f4b0b702d495bebe8
Antoine Collas <contact@antoinecollas.fr>
2022-01-20 11:11:59 +0100

    add code to generate S and D with sampling without replacement

--- utils.py
+++ utils.py
@@ -8,4 +8,16 @@ def create_S_D(X, y, num_const):
     dd = (y[k1] != y[k2])
     S = X[k1[ss]] - X[k2[ss]]
     D = X[k1[dd]] - X[k2[dd]]
+
+    # other way to create S and D
+    # k1 = np.arange(X.shape[0])
+    # k2 = np.arange(X.shape[0])
+    # rnd.shuffle(k1)
+    # rnd.shuffle(k2)
+    # k1 = k1[:num_const]
+    # k2 = k2[:num_const]
+    # ss = (y[k1] == y[k2])
+    # dd = (y[k1] != y[k2])
+    # S = X[k1[ss]] - X[k2[ss]]
+    # D = X[k1[dd]] - X[k2[dd]]
     return S, D
