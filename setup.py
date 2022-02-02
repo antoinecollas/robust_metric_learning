b0b0753b02aee8c87c23148ce76fd7617e98e728
Antoine Collas <contact@antoinecollas.fr>
2022-02-02 10:32:32 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='robust_metric_learning',
+    version='0.0',
+    description='Robust metric learning',
+    url='https://github.com/antoinecollas/robust_metric_learning',
+    author='Antoine Collas',
+    keywords='Riemann, Riemannian, geometry, metric learning',
+    packages=find_packages(),
+    python_requires='>=3.7',
+)
+
